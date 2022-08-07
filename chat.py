d945eaacdc31f279b34f01f3cab484a825d40042
mayosen <gare577@gmail.com>
2022-08-07 19:15:25 +0300

    Add watchdog and logger

--- chat.py
+++ chat.py
@@ -1,5 +1,7 @@
 import asyncio
 import json
+import logging
+import os.path
 from argparse import ArgumentParser
 from os import environ
 from asyncio import Queue
@@ -7,13 +9,11 @@ from tkinter import messagebox
 
 import aiofiles
 
-from gui import draw, NicknameReceived, SendingConnectionStateChanged, ReadConnectionStateChanged
+from gui import draw, NicknameReceived, SendingConnectionStateChanged, ReadConnectionStateChanged, TkAppClosed
 from utils import open_connection, decode, format_log, encode
 
 
 # TODO: README
-# TODO: Писать в логи статусы
-# TODO: Убрать отладочные print
 
 
 class InvalidToken(Exception):
@@ -39,7 +39,9 @@ def parse_config():
     return host, listen_port, send_port, path, user_hash
 
 
-async def read_messages(host: str, port: int, messages_queue: Queue, save_queue: Queue, updates: Queue):
+async def read_messages(
+        host: str, port: int, messages_queue: Queue, save_queue: Queue, updates: Queue, watchdog: Queue
+):
     updates.put_nowait(ReadConnectionStateChanged.INITIATED)
 
     async with open_connection(host, port) as (reader, writer):
@@ -47,8 +49,21 @@ async def read_messages(host: str, port: int, messages_queue: Queue, save_queue:
 
         while True:
             message = decode(await reader.readline())
+            watchdog.put_nowait(f"New message in chat")
             messages_queue.put_nowait(message)
             save_queue.put_nowait(message)
+
+
+def read_history(filepath: str):
+    filename = f"{filepath}/logs.txt"
+
+    if not os.path.exists(filename):
+        with open(filename, "w"):
+            pass
+
+    with open(filename, "r") as logs:
+        messages = [line.rstrip("\n") for line in logs.readlines()]
+        return messages
 
 
 async def save_messages(filepath: str, save_queue: Queue):
@@ -58,18 +73,15 @@ async def save_messages(filepath: str, save_queue: Queue):
             await logs.write(format_log(message))
 
 
-def read_history(filepath: str):
-    with open(f"{filepath}/logs.txt", "a+") as logs:
-        messages = [line.rstrip("\n") for line in logs.readlines()]
-        return messages
-
-
-async def send_messages(host: str, port: int, user_hash: str, sending_queue: Queue, updates: Queue):
+async def send_messages(
+        host: str, port: int, user_hash: str, sending_queue: Queue, updates: Queue, watchdog: Queue
+):
     updates.put_nowait(SendingConnectionStateChanged.INITIATED)
 
     async with open_connection(host, port) as (reader, writer):
         updates.put_nowait(SendingConnectionStateChanged.ESTABLISHED)
         await reader.readline()  # Enter hash
+        watchdog.put_nowait("Prompt before auth")
         writer.write(encode(user_hash))
         await writer.drain()
 
@@ -79,36 +91,55 @@ async def send_messages(host: str, port: int, user_hash: str, sending_queue: Que
         if not user_info:
             raise InvalidToken
 
+        watchdog.put_nowait("Authorization done")
         updates.put_nowait(NicknameReceived(user_info["nickname"]))
-        print(f"Выполнена авторизация. Пользователь {user_info['nickname']}.")
 
         while True:
             message = await sending_queue.get()
-            print(f"Пользователь ввел: {message}")
             message = message.replace("\n", "")
             writer.write(encode(f"{message}\n"))
+            watchdog.put_nowait("Message sent")
             await writer.drain()
 
 
+async def watch_for_connection(watchdog_queue: Queue):
+    logger = logging.getLogger("watchdog_logger")
+
+    while True:
+        event = await watchdog_queue.get()
+        logger.debug(event)
+
+
 async def main():
+    logging.basicConfig(
+        format="[%(asctime)s.%(msecs).03d] %(levelname)s [%(name)s] %(message)s",
+        datefmt="%H:%M:%S",
+        level=logging.DEBUG,
+    )
     host, listen_port, send_port, filepath, user_hash = parse_config()
 
     messages_queue = Queue()
     sending_queue = Queue()
     status_updates_queue = Queue()
     save_queue = Queue()
+    watchdog_queue = Queue()
     history = read_history(filepath)
 
     try:
         await asyncio.gather(
             draw(history, messages_queue, sending_queue, status_updates_queue),
-            read_messages(host, listen_port, messages_queue, save_queue, status_updates_queue),
+            read_messages(host, listen_port, messages_queue, save_queue, status_updates_queue, watchdog_queue),
             save_messages(filepath, save_queue),
-            send_messages(host, send_port, user_hash, sending_queue, status_updates_queue),
+            send_messages(host, send_port, user_hash, sending_queue, status_updates_queue, watchdog_queue),
+            watch_for_connection(watchdog_queue),
         )
     except InvalidToken:
-        messagebox.showerror("Неверный токен", "Проверьте токен, сервер его не узнал")
+        messagebox.showwarning("Неверный токен", "Проверьте токен, сервер его не узнал")
         return
+    except TkAppClosed:
+        # TODO: Обработка
+        logging.debug("Клиент закрыт")
+        pass
 
 
 asyncio.run(main())
